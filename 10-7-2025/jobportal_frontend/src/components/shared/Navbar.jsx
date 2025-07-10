3ef32f7110261c4d44ac7bc4006fdea4c791ab24
nitin chauhan <nitinkiranchauhan@gmail.com>
2025-07-10 10:20:03 +0530

    with frontend

---
+++ 10-7-2025/jobportal_frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Container, Nav, Navbar, Dropdown, Image } from "react-bootstrap";
+
+export default function CustomNavbar() {
+  let user = {
+    name: "nitin",
+    role: "recruiter",
+  };
+  //   user = false;
+  return (
+    <>
+      <Navbar expand="lg" className="shadow-sm">
+        <Container>
+          <Navbar.Brand href="#home" className="display-1 fs-1">
+            Job <span className="text-danger">Portal</span>
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          {/*links navigation*/}
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto gap-3 fw-medium">
+              {user && user.role === "recruiter" ? (
+                <>
+                  <Nav.Link to={""}>Companies</Nav.Link>
+                  <Nav.Link to={""}>Jobs</Nav.Link>
+                </>
+              ) : (
+                <>
+                  <Nav.Link to={""}>Home</Nav.Link>
+                  <Nav.Link to={""}>Jobs</Nav.Link>
+                  <Nav.Link to={""}>Browse</Nav.Link>
+                </>
+              )}
+            </Nav>
+
+            {!user ? (
+              <>
+                <button>Login</button>
+                <button>Singup</button>
+              </>
+            ) : (
+              <>
+                <>
+                  <Dropdown>
+                    <Dropdown.Toggle variant="link" className="p-0 border-0">
+                      <Image
+                        src={
+                          "https://th.bing.com/th?id=ORMS.d0cbfecd5b58e76aed2fd46e5177728a&pid=Wdp&w=612&h=304&qlt=90&c=1&rs=1&dpr=1.375&p=0"
+                        }
+                        roundedCircle
+                        width={40}
+                        height={40}
+                      />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu className="p-3" style={{ width: "5rem" }}>
+                      <div className="d-flex align-item-center justify-content-center mb-2">
+                        {user.name}
+                      </div>
+                      <div className="d-flex align-item-center justify-content-center mb-2">
+                        {user.role}
+                      </div>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </>
+              </>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
